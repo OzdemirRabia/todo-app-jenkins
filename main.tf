bda1783a07c5129a09a11bb12a1f0ed93a7c907c
Rabia <hnt.ozdmr@gmail.com>
2022-07-14 04:35:30 +0300

    files

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ resource "aws_instance" "managed_nodes" {
   instance_type = "t2.micro"
   key_name = "firstkey"
   vpc_security_group_ids = [aws_security_group.tf-sec-gr.id]
-  iam_instance_profile = "jenkins-project-profile"
+  iam_instance_profile = "jenkins-project-profile3"
   tags = {
     Name = "ansible_${element(var.tags, count.index )}"
     stack = "ansible_project"
